2bb076685b415038bc0890e8fad97a108aaf1ffe
KhaetbekIT <hayotbek-web@mail.ru>
2025-05-12 00:22:02 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: "export", // Required for static export
+	// Optional for project sites:
+	basePath: process.env.NODE_ENV === "production" ? "/Forest-Travel" : "",
 };
 
 export default nextConfig;
