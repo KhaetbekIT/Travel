ad66e00a389745b4b79fff3cc25558e1cad04517
KhaetbekIT <hayotbek-web@mail.ru>
2025-05-25 14:07:44 +0500

    remove project

--- src/configs/router.config.ts
+++
@@ -1,8 +0,0 @@
-export const RouterConfig = {
-	homePage: "/",
-	aboutPage: "/about",
-	contactPage: "/contact",
-	notFoundPage: "/404",
-	toursPage: "/pages/tours",
-	blogByIdPage: (id: number) => `/pages/blogs/${id}`,
-};
